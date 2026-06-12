6f4dd6f912f3845aaa186bdeee5a1d1eaed1bd16
Ruslan Nakhmedov <ruslan-nahmedov@mail.ru>
2026-06-12 14:47:05 +0300

    update category.py and product.py update tests/

--- src/product.py
+++ src/product.py
@@ -1,14 +1,35 @@
 class Product:
     """Класс для товаров"""
-    name: str
-    description: str
-    price: float
-    quantity: int
-
     def __init__(
         self, name: str, description: str, price: float, quantity: int
     ) -> None:
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_data: dict, products: list = None):
+        if products is not None:
+            for product in products:
+                if product.name == product_data["name"]:
+                    product.quantity += product_data["quantity"]
+                    product.price = max(product.price, product_data["price"])
+                    return product
+
+        return cls(
+            product_data["name"],
+            product_data["description"],
+            product_data["price"],
+            product_data["quantity"])
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, price: float):
+        if price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = price
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,10 +7,14 @@ from src.product import Product
 @pytest.fixture
 def test_product():
     return Product(
-        name="Test Product", description="Test Description", price=100, quantity=10
-    )
+        name="Test Product", description="Test Description", price=100, quantity=10)
 
 
 @pytest.fixture
 def test_category(test_product):
     return Category("Test Category", "Test Description", products=[test_product])
+
+@pytest.fixture
+def test_product_price_lower():
+    return Product(
+        name="Test Product", description="Test Description", price=100, quantity=10)
